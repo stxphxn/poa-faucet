8340ff960da53fa2344efe0a6a727814795d6644
stxphxn <stephen.horsfall@atlascity.io>
2020-04-20 13:54:32 +0100

    add catalyst colours

--- public/assets/javascripts/application/index.js
+++ public/assets/javascripts/application/index.js
@@ -1,34 +1,33 @@
-$(function() {
-	var loader = $(".loading-container");
-	$( "#faucetForm" ).submit(function( e ) {
-		e.preventDefault();
+$(() => {
+  const loader = $('.loading-container');
+  $('#faucetForm').submit(function (e) {
+    e.preventDefault();
     	$this = $(this);
-		loader.removeClass("hidden");
-		var receiver = $("#receiver").val();
-		$.ajax({
-		  	url:"/",
-		  	type:"POST",
-		  	data: $this.serialize()
-		}).done(function(data) {
-			grecaptcha.reset();
-			if (!data.success) {
-				loader.addClass("hidden");
-				console.log(data)
-				console.log(data.error)
-				swal("Error", data.error.message, "error");
-				return;
-			}
+    loader.removeClass('hidden');
+    const receiver = $('#receiver').val();
+    $.ajax({
+		  	url: '/',
+		  	type: 'POST',
+		  	data: $this.serialize(),
+    }).done((data) => {
+      grecaptcha.reset();
+      if (!data.success) {
+        loader.addClass('hidden');
+        console.log(data);
+        console.log(data.error);
+        swal('Error', data.error.message, 'error');
+        return;
+      }
 
-			$("#receiver").val('');
-			loader.addClass("hidden");
-			swal("Success",
-			  `0.5 SPOA has been successfully transferred to <a href="https://blockscout.com/poa/sokol/tx/${data.success.txHash}" target="blank">${receiver}</a>`,
-			  "success"
-			);
-		}).fail(function(err) {
-			grecaptcha.reset();
-			console.log(err);
-			loader.addClass("hidden");
-		});
-	});
+      $('#receiver').val('');
+      loader.addClass('hidden');
+      swal('Success',
+			  `1 tKAT has been successfully transferred to <a href="http://catalystexplorer.z33.web.core.windows.net/tx/${data.success.txHash}" target="blank">${receiver}</a>`,
+			  'success');
+    }).fail((err) => {
+      grecaptcha.reset();
+      console.log(err);
+      loader.addClass('hidden');
+    });
+  });
 });
